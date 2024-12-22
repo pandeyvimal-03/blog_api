06441e21ff1c7467c8eff766fcd87b08c652e4cf
pandeyvimal <pandeyvimal2002@gmil.com>
2024-12-22 18:05:31 +0530

    setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import config from "./config.js";
+
+const app = express()
+
+app.get("/" , (req , res)=>{
+    res.send({msg : "You are connected with server"})
+})
+
+app.listen(config.port , ()=>{
+    console.log("server started successfully")
+})
