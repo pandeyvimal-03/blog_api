13fe3755bad3d0d01448c8e9a147b72c016adcc9
Vimal Pandey <100365060+pandeyvimal-03@users.noreply.github.com>
2024-12-22 21:28:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import config from "./config.js";
 const app = express()
 
 app.get("/" , (req , res)=>{
-    res.send({msg : "You are connected with server"})
+     res.send({msg : "You are connected with server"})
+})
+
+app.get("/services" , (req , res)=>{
+     res.send({services : "List of services are not available now"})
 })
 
 app.listen(config.port , ()=>{
